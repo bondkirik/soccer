8b375867c04cbfabdfedc0c251523be3a19f4e6b
Kirils Bondarevs <bondarevs@co.inbox.lv>
2021-05-05 11:58:03 +0300

    init commit

---
+++ database/migrations/2021_05_05_085127_create_division_team_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDivisionTeamTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('division_team', function (Blueprint $table) {
+            $table->integer('division_id');
+            $table->integer('team_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('division_team');
+    }
+}
